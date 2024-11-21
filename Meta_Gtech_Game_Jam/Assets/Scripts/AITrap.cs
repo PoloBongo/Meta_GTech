b18f297558ff23b5315d804cf7f7f6c3a11dc0ad
Arthur <earthrp.polo@gmail.com>
2024-11-21 21:10:45 +0100

    [ADD] somes package & android build

--- Meta_Gtech_Game_Jam/Assets/Scripts/AITrap.cs
+++ Meta_Gtech_Game_Jam/Assets/Scripts/AITrap.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AITrap : MonoBehaviour
 {
-    
+    [SerializeField] private List<GameObject> traps;
+    [SerializeField] private Transform trapPlacementTest;
+
+    private void Start()
+    {
+        PlaceTrap();
+    }
+
+    private void PlaceTrap()
+    {
+        Instantiate(traps[0], trapPlacementTest.position, trapPlacementTest.rotation);
+    }
 }
