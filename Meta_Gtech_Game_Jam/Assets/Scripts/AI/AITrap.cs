f67f29ba1abe38e2ca6d8febf31d282299194d0b
46Yuu <htran@gaming.tech>
2024-11-23 03:21:54 +0100

    Update AITrap.cs

--- Meta_Gtech_Game_Jam/Assets/Scripts/AI/AITrap.cs
+++ Meta_Gtech_Game_Jam/Assets/Scripts/AI/AITrap.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AITrap : MonoBehaviour
@@ -7,6 +8,10 @@ public class AITrap : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private List<GameObject> trapAIPrefab;
     [SerializeField] private RaycastingDetectionObject raycastingDetectionObject;
+    private List<GameObject> trapAIlist1 = new List<GameObject>();
+    private List<GameObject> trapAIlist2 = new List<GameObject>();
+    private List<GameObject> trapAIlist3 = new List<GameObject>();
+    [SerializeField] private int numberOfTraps;
     
     [Header("Settings Trap")]
     [SerializeField] private float spawnDistanceToPlayer;
@@ -23,6 +28,82 @@ public class AITrap : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
         raycastingDetectionObject = GetComponent<RaycastingDetectionObject>();
         listTrapCount = trapAIPrefab.Count;
+        SetupPools();
+    }
+    private void SetupPools()
+    {
+        trapAIlist1.Clear();
+        trapAIlist2.Clear();
+        trapAIlist3.Clear();
+        for (int i = 0; i < numberOfTraps; i++)
+        {
+            GameObject trap = Instantiate(trapAIPrefab[0], transform);
+            trapAIlist1.Add(trap);
+            trap.SetActive(false);
+        }
+        for (int i = 0; i < numberOfTraps; i++)
+        {
+            GameObject trap = Instantiate(trapAIPrefab[1], transform);
+            trapAIlist2.Add(trap);
+            trap.SetActive(false);
+        }
+        for (int i = 0; i < numberOfTraps; i++)
+        {
+            GameObject trap = Instantiate(trapAIPrefab[2], transform);
+            trapAIlist3.Add(trap);
+            trap.SetActive(false);
+        }
+    }
+
+    public GameObject GetFirstAvailableTrap(int whichTrap)
+    {
+        if (whichTrap == 0)
+        {
+            foreach (var trap in trapAIlist1)
+            {
+                if(trap.activeInHierarchy) continue;
+                return trap;
+            }
+
+            for (int i = 0; i < numberOfTraps / 2; i++)
+            {
+                GameObject trap = Instantiate(trapAIPrefab[0], transform);
+                trapAIlist1.Add(trap);
+                trap.SetActive(false);
+            }
+            return trapAIlist1.Last();
+        }
+
+        if (whichTrap == 1)
+        {
+            foreach (var trap in trapAIlist2)
+            {
+                if(trap.activeInHierarchy) continue;
+                return trap;
+            }
+
+            for (int i = 0; i < numberOfTraps / 2; i++)
+            {
+                GameObject trap = Instantiate(trapAIPrefab[1], transform);
+                trapAIlist2.Add(trap);
+                trap.SetActive(false);
+            }
+            return trapAIlist2.Last();
+        }
+        foreach (var trap in trapAIlist3)
+        {
+            if (trap.activeInHierarchy) continue;
+            return trap;
+        }
+
+        for (int i = 0; i < numberOfTraps / 2; i++)
+        {
+            GameObject trap = Instantiate(trapAIPrefab[2], transform);
+            trapAIlist3.Add(trap);
+            trap.SetActive(false);
+        }
+
+        return trapAIlist3.Last();
     }
 
     private void PlaceSingleTrap()
@@ -37,7 +118,8 @@ public class AITrap : MonoBehaviour
 
             Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
             int newTrap = Random.Range(0, listTrapCount);
-            Instantiate(trapAIPrefab[newTrap], spawnPos, trapAIPrefab[newTrap].transform.rotation);
+            GameObject trap = GetFirstAvailableTrap(newTrap);
+            trap.transform.position = spawnPos;
         }
     }
     
